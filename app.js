04d3790c314ca21b28298213b56f991e3070cb9a
PW <mauztwitch@hotmail.com>
2016-03-09 20:13:25 -0500

    zzz

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/planner-leader', plannerLeader);
-app.use('/planner-leader', plannerMember);
+app.use('/planner-member', plannerMember);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/plannerleader.js
+++ routes/plannerleader.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/planner-leader', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('planner-leader', { title: 'HuddleHere ALPHA' });
 });
 
