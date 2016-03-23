a490463f70fd392c1effb22f66ba9e2a0edd2fc1
PW <mauztwitch@hotmail.com>
2016-03-23 16:26:43 -0400

    Write initial Mongo schema

--- models/db.js
+++ models/db.js
@@ -1,1 +1,42 @@
 var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/HuddleHere';
+mongoose.connect(dbURI);
+
+// status messages to console
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose disconnected from db');
+});
+
+// function encapsulation for cleaning up connections,
+// as given in the Getting MEAN text
+var gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected: ' + msg);
+  });
+};
+
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
+});
