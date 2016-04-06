066052321c51d659155e4cdc5872dd51081d8a20
PW <mauztwitch@hotmail.com>
2016-04-06 00:09:15 -0400

    Fix reverse geocoding on coach page

--- public/javascripts/coachLoc.js
+++ public/javascripts/coachLoc.js
@@ -5,18 +5,13 @@
  * Sets meeting location based on meeting location data from the
  * HuddleHere API
  */
-var populateLocation = function() {
-  var parsedAddress = codeLatLng(objMeeting.location[0], objMeeting.location[1]),
-    parts;
-    
-  if (typeof(parsedAddress) === "string" && parsedAddress.length > 0) {
-    // parts[0] = street address
-    // parts[1] = city
-    // parts[2] = State ZIP
-    parts = parsedAddress.split(",");
-  }
-  
-  console.log("parsedAddress is: " + parsedAddress);
+var populateLocation = function(strDecodedAddress) {
+  var parts;
+
+  // parts[0] = street address
+  // parts[1] = city
+  // parts[2] = State ZIP
+  parts = strDecodedAddress.split(",");
   
   $("#streetAddress").text(parts[0]);
   $("#cityState").text(parts[1] + "," + parts[2]);
--- public/javascripts/geo.js
+++ public/javascripts/geo.js
@@ -8,9 +8,29 @@
 
 var utilGeocoder;
 
+/**
+ * Function: populateLocation
+ * 
+ * Populates location with formatted address string returned by
+ */
+var populateLocation = function(strDecodedAddress) {
+  var parts;
+
+  // parts[0] = street address
+  // parts[1] = city
+  // parts[2] = State ZIP
+  if (strDecodedAddress.length > 0) {
+    parts = strDecodedAddress.split(",");
+    $("#streetAddress").text(parts[0]);
+    $("#cityState").text(parts[1] + "," + parts[2]);
+  } else {
+    $("#streetAddress").text("NOT FOUND");
+  }
+};
+
 var geoInitialize = function() {
   utilGeocoder = new google.maps.Geocoder();
-  populateLocation();
+  codeLatLng(objMeeting.location[0], objMeeting.location[1]);
 };
 
 var codeLatLng = function(lat, lng) {
@@ -20,14 +40,16 @@ var codeLatLng = function(lat, lng) {
   }, function (results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       if (results[1]) {
-        return results[1].formatted_address;
+        console.log('Target result: ' + results[1]);
+        console.log('Formatted: ' + results[1].formatted_address);
+        populateLocation(results[1].formatted_address);
       } else {
         console.log('GEOCODER: No results found');
-        return "";
+        populateLocation("");
       }
     } else {
       console.log('Geocoder failed due to: ' + status);
-      return "";
+      populateLocation("");
     }
   });
 };
