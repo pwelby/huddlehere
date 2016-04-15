9016aa21e38a030cabfde589f5cd8eff71facfe8
PW <mauztwitch@hotmail.com>
2016-04-15 17:51:29 -0400

    Implement member submit form posting

--- public/javascripts/memberPost.js
+++ public/javascripts/memberPost.js
@@ -5,6 +5,43 @@
  * Handles member availability status updates
  */
 
-(function(){
-  var 
-}());
+// TODO(Peter): Add validation here so that we don't get messed up by
+// unselected members or empty :selected collections
+
+$('#submitForm').click(function (e) {
+  var postData = Object.assign({}, objMeeting),
+    i, formPutRequest = new XMLHttpRequest(),
+    selMember = $("#memberPicker").val(),
+    currStatus;
+  var meetingId = postData._id;
+  var apiPath = "/api/meetings/" + meetingId;
+  
+  if ($("#statAvailable").is(":selected")) {
+    currStatus = "Available";
+  } else {
+    currStatus = "Unavailable";
+  }
+  
+  formPutRequest.open("PUT", window.location.protocol + "//" + window.location.host + apiPath);
+  formPutRequest.setRequestHeader("Content-Type", "application/json");
+  formPutRequest.responseType = "json";
+  
+  for (i = 0; i < postData.members.length; i++) {
+    if (selMember === postData.members[i].split(';')[0]) {
+      postData.members[i] = postData.members[i].split(';')[0] + ";" + currStatus;
+    }
+  }
+  console.log("postData:");
+  console.log(postData);
+  
+  formPutRequest.onreadystatechange = function() {
+    if(formPutRequest.readyState === XMLHttpRequest.DONE && formPutRequest.status === 200) {
+      window.alert("Status updated successfully!");
+    }
+  };
+  
+  console.log("request:");
+  console.log(formPutRequest);
+  
+  formPutRequest.send(postData);
+});
