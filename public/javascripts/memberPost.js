9a6ba0a7f4ead939f17dcac25b07f316806eb3c1
PW <mauztwitch@hotmail.com>
2016-04-16 12:16:37 -0400

    Fix request construction

--- public/javascripts/memberPost.js
+++ public/javascripts/memberPost.js
@@ -12,36 +12,68 @@ $('#submitForm').click(function (e) {
   var postData = Object.assign({}, objMeeting),
     i, formPutRequest = new XMLHttpRequest(),
     selMember = $("#memberPicker").val(),
-    currStatus;
+    currStatus, requestParams = "", prop, arrMembers;
+    
   var meetingId = postData._id;
+  var requestUrl = window.location.protocol + "//" + window.location.host;
   var apiPath = "/api/meetings/" + meetingId;
   
+  // get current status
   if ($("#statAvailable").is(":selected")) {
     currStatus = "Available";
   } else {
     currStatus = "Unavailable";
   }
   
-  formPutRequest.open("PUT", window.location.protocol + "//" + window.location.host + apiPath);
-  formPutRequest.setRequestHeader("Content-Type", "application/json");
-  formPutRequest.responseType = "json";
+  console.log("currStatus: " + currStatus);
+  console.log("selMember: " + selMember);
   
+  // update selected member's status
   for (i = 0; i < postData.members.length; i++) {
     if (selMember === postData.members[i].split(';')[0]) {
       postData.members[i] = postData.members[i].split(';')[0] + ";" + currStatus;
     }
   }
-  console.log("postData:");
-  console.log(postData);
   
+  // reconstruct the comma-separated members list to send to API
+  arrMembers = postData.members;
+  postData.members = "";
+  for (i = 0; i < arrMembers.length; i++) {
+    postData.members += arrMembers[i] + ",";
+  }
+  // remove the trailing ","
+  postData.members = postData.members.substr(0, postData.members.length - 1);
+  console.log("reconstructed member string: " + postData.members);
+  
+  // reconstruct the comma-separated location array to send to API
+  postData.location = "" + postData.location[0] + "," + postData.location[1];
+  console.log("reconstructed location string: " + postData.location);
+
+  formPutRequest.open("PUT", requestUrl + apiPath);
   formPutRequest.onreadystatechange = function() {
     if(formPutRequest.readyState === XMLHttpRequest.DONE && formPutRequest.status === 200) {
       window.alert("Status updated successfully!");
+      console.log("Request succeeded:");
+      console.log(formPutRequest.responseText);
+    } else if (formPutRequest.status !== 200) {
+      console.log("Request failed: ");
+      console.log(formPutRequest.readyState, formPutRequest.status);
     }
   };
   
-  console.log("request:");
-  console.log(formPutRequest);
   
-  formPutRequest.send(postData);
+  
+  // serialize postData properties for URL encoding
+  for (prop in postData) {
+    if (postData.hasOwnProperty(prop)) {
+      requestParams += prop + "=" + postData[prop] + "&";
+    }
+  }
+  // remove the trailing "&"
+  requestParams = requestParams.substr(0, requestParams.length - 1);
+  console.log("requestParams: " + requestParams);
+  
+  formPutRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  formPutRequest.responseType = "json";
+  formPutRequest.send(requestParams);
 });
