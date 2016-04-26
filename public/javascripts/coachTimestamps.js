2845c8e625fdb61a1fca0d7c52c76dbd701e2d78
PW <mauztwitch@hotmail.com>
2016-04-25 21:02:09 -0400

    Prototype material design conversion with working radio buttons

---
+++ public/javascripts/coachTimestamps.js
@@ -0,0 +1,7 @@
+/**
+ * File: /public/javascripts/coachTimestamps.js
+ * 
+ * Constructs agenda item timestamps from the meeting start time 
+ * and the provided agenda item durations (in minutes)
+ */
+
